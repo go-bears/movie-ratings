02d3a87afeeb221f1e4cdbf33a0e570d446b1857
Hackbright Student <info@hackbrightacademy.com>
2016-02-03 18:06:36 -0800

    working: user_list and user_page, movie_list, movie detail | working submiting new ratings

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Ratings, Movie, connect_to_db, db
 
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -22,12 +24,13 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return render_template("homepage.html")
+
+    return render_template("homepage.html", logged_in=session.get('email', False))
 
 
 @app.route('/login_completion', methods=["POST"])
 def login_completion():
-    """Homepage."""
+    """Login resolution page, takes in login info, checks db for login or adds user"""
 
     email = request.form.get('username')
     password =request.form.get('password')
@@ -39,12 +42,12 @@ def login_completion():
         print "Database queried!"
         flash("You are now logged in!")
         session['email'] = email
-        return render_template("homepage.html")
+        return render_template("homepage.html", logged_in=session.get('email', False))
 
     # email already exists -- incorrect password
     elif db.session.query(User).filter(User.email==email).first():
         flash("The password does not match the user email. Try Again!")
-        return render_template('login_form.html') 
+        return render_template('login_form.html', logged_in=session.get('email', False)) 
 
     # no email in db -- new user add
     elif not db.session.query(User).filter((User.email==email) & (User.password==password)).first():
@@ -56,7 +59,7 @@ def login_completion():
         flash("Hi! We added you the database")
 
         print "I commited ", new_user, "to the database"
-        return render_template("homepage.html")
+        return render_template("homepage.html", logged_in=session.get('email', False))
     
 
 @app.route("/log_out")
@@ -65,8 +68,8 @@ def logging_out():
     
     session.pop('email', None)
     flash("Thanks! You are now logged out")
-        
-    return render_template("homepage.html")
+
+    return render_template("homepage.html", logged_in=session.get('email', False))
 
 
 @app.route("/login")
@@ -74,7 +77,7 @@ def login_form():
     """Displays user login form."""
 
     
-    return render_template("login_form.html")
+    return render_template("login_form.html", logged_in=session.get('email', False))
 
 
 @app.route("/users")
@@ -82,7 +85,87 @@ def user_list():
     """Show list of users."""
 
     users = User.query.all()
-    return render_template("user_list.html", users=users)
+    return render_template("user_list.html", users=users, logged_in=session.get('email', False))
+
+
+@app.route("/users/<int:user_id>")
+def user_page(user_id):
+    """Displays User profile information and movies they've rated"""
+
+    # fetch the user based on their ID
+    user = db.session.query(User).get(user_id)
+
+    # unpack info from the user instance by checking attributes
+    zipcode = user.zipcode
+    age = user.age
+
+    #list of ratings for the user. navigating attributes from user to ratings
+    ratings_list = user.ratings
+
+    # render these things to user_page.html: zipcode and age
+    # (2) pass ratings associated with user_id  and pass list to jinga template on 
+    # user_page.html. And have jinga unpack the  movie title
+
+    return render_template("user_page.html", zipcode=zipcode,
+                                             age=age,
+                                             ratings_list=ratings_list,
+                                             logged_in=session.get('email', False))
+
+# make a page to list all movie :pass list
+
+# set a route to all_movies_pages
+
+# query db to get all movies
+@app.route("/movies")
+def movies_list():
+    """Show list of movies."""
+
+    movies = Movie.query.order_by(Movie.title).all()
+    return render_template("movie_list.html", movies=movies, logged_in=session.get('email', False))
+
+
+# make  details page for each movie; page displays all ratings, imdb url
+
+# check Flask session to check if user is logged in:
+#   if yes: able to set a rating or update current rating
+ 
+@app.route("/movies/<int:movie_id>")
+def movie_detail(movie_id):
+    """Displays Movie detail information; logged_in user can rate or update movie ratings"""
+
+    # fetch the movie details by movie_ID
+    movie = db.session.query(Movie).get(movie_id)
+
+    title = movie.title
+    release_date = movie.release_date.strftime('%B %d, %Y')
+    imdb_url = movie.imdb_url
+    ratings_list = movie.ratings
+
+    total = sum(r.score for r in ratings_list)
+
+    avg_rating = float(total / len(ratings_list))
+
+    user = db.session.query(User).filter(User.email==session['email']).one()
+
+    user_rating = None
+
+    for r in user.ratings:
+        if r.movie_id == movie_id:
+            user_rating = r.score
+
+    return render_template("movie_page.html", title=title,
+                                              release_date=release_date,
+                                              imdb_url=imdb_url,
+                                              avg_rating=avg_rating,
+                                              user_rating = user_rating,
+                                              ratings_list=ratings_list,
+                                              movie_id = movie_id,
+                                              logged_in=session.get('email', False))
+
+
+@app.route("/new_rating/<int:movie_id>")
+def update_rating(movie_id):
+    pass
 
 
 
